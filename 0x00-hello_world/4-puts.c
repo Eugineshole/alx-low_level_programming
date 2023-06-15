31aa4d3ebbe5b094d9d9f0f784a493506c8e4322
eugineshole <eugineshole122@gmail.com>
2023-06-15 16:53:24 -0700

    Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#iclude<stdio h>
+/**
+ * main Entry point
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like bulilding a multilingual puzzle").
+	return (0),
+)	          	       
